3c64da142e69ae7c9d83a8674befa293bff8d6bf
Shashank Gupta <shashankgupta3@gmail.com>
2017-02-14 13:38:24 -0500

    fix grid

--- src/Root.js
+++ src/Root.js
@@ -5,21 +5,17 @@ import YtSearch from './YtSearch'
 export default class Root extends React.Component {
   render() {
     return (
-      <Grid>
-        <Grid.Column mobile={1} tablet={2} computer={2} />
-        <Grid.Column mobile={14} tablet={6} computer={6}>
+      <Grid centered columns={2}>
+        <Grid.Column mobile={14} tablet={7} computer={7}>
           <Segment>
             Manage Playlists
           </Segment>
         </Grid.Column>
-        <Grid.Column mobile={1} only='mobile' />
-        <Grid.Column mobile={1} only='mobile' />
-        <Grid.Column mobile={14} tablet={6} computer={6}>
+        <Grid.Column mobile={14} tablet={7} computer={7}>
           <Segment>
             <YtSearch />
           </Segment>
         </Grid.Column>
-        <Grid.Column mobile={1} tablet={2} computer={2} />
       </Grid>
     );
   }
